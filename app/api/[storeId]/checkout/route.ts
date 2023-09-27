9b3e682d1418c9376b9622c9220afb2193920561
wubbzy25 <carlosasalas321@gmail.com>
2023-09-27 12:05:38 -0500

    change the stripe

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -1,7 +1,5 @@
-import Stripe from "stripe";
 import { NextResponse } from "next/server";
 
-import { stripe } from "@/lib/stripe";
 import prismadb from "@/lib/prismadb";
 
 const corsHeaders = {
@@ -24,29 +22,6 @@ export async function POST(
     return new NextResponse("Product ids are required", { status: 400 });
   }
 
-  const products = await prismadb.product.findMany({
-    where: {
-      id: {
-        in: productIds
-      }
-    }
-  });
-
-  const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
-
-  products.forEach((product) => {
-    line_items.push({
-      quantity: 1,
-      price_data: {
-        currency: 'USD',
-        product_data: {
-          name: product.name,
-        },
-        unit_amount: product.price.toNumber() * 100
-      }
-    });
-  });
-
   const order = await prismadb.order.create({
     data: {
       storeId: params.storeId,
@@ -55,29 +30,18 @@ export async function POST(
         create: productIds.map((productId: string) => ({
           product: {
             connect: {
-              id: productId
-            }
-          }
-        }))
-      }
+              id: productId,
+            },
+          },
+        })),
+      },
+    },
+  });
+
+  return NextResponse.json(
+    { orderId: order.id },
+    {
+      headers: corsHeaders,
     }
-  });
-
-  const session = await stripe.checkout.sessions.create({
-    line_items,
-    mode: 'payment',
-    billing_address_collection: 'required',
-    phone_number_collection: {
-      enabled: true,
-    },
-    success_url: `${process.env.FRONTEND_STORE_URL}/cart?success=1`,
-    cancel_url: `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
-    metadata: {
-      orderId: order.id
-    },
-  });
-
-  return NextResponse.json({ url: session.url }, {
-    headers: corsHeaders
-  });
-};
+  );
+}
