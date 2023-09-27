cebaef16a056b66e37c0726c1fd5b24cc570a8a1
Carlos Andres Salas Correa <83189141+wubbzy25@users.noreply.github.com>
2023-09-27 16:13:21 -0500

    Update route.ts

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -38,7 +38,7 @@ export async function POST(
     line_items.push({
       quantity: 1,
       price_data: {
-        currency: "COP",
+        currency: "USD",
         product_data: {
           name: product.name,
         },
