db3f75263a62f08eea562b36dabf4088ef64ee79
wubbzy25 <carlosasalas321@gmail.com>
2023-10-24 09:50:10 -0500

    api update

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -1,12 +1,9 @@
-import Stripe from "stripe";
 import { NextResponse } from "next/server";
-
-import { stripe } from "@/lib/stripe";
 import prismadb from "@/lib/prismadb";
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Methods": "POST, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
@@ -14,73 +11,41 @@ export async function OPTIONS() {
   return NextResponse.json({}, { headers: corsHeaders });
 }
 
-export async function POST(
-  req: Request,
-  { params }: { params: { storeId: string } }
-) {
-  const { productIds } = await req.json();
+export async function POST(req) {
+  const { address, phoneNumber, paymentMethod, productIds } = await req.json();
 
-  if (!productIds || productIds.length === 0) {
-    return new NextResponse("Product ids are required", { status: 400 });
+  if (!address || !phoneNumber || !paymentMethod || !productIds || productIds.length === 0) {
+    return new NextResponse("Incomplete or invalid data", { status: 400 });
   }
 
-  const products = await prismadb.product.findMany({
-    where: {
-      id: {
-        in: productIds,
-      },
-    },
-  });
-
-  const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
-
-  products.forEach((product) => {
-    line_items.push({
-      quantity: 1,
-      price_data: {
-        currency: "USD",
-        product_data: {
-          name: product.name,
+  try {
+    // Guarda los datos del formulario en tu base de datos
+    const order = await prismadb.order.create({
+      data: {
+        address,
+        phoneNumber,
+        paymentMethod,
+        isPaid: false,
+        orderItems: {
+          create: productIds.map((productId) => ({
+            product: {
+              connect: {
+                id: productId,
+              },
+            },
+          })),
         },
-        unit_amount: product.price.toNumber() * 100,
       },
     });
-  });
 
-  const order = await prismadb.order.create({
-    data: {
-      storeId: params.storeId,
-      isPaid: false,
-      orderItems: {
-        create: productIds.map((productId: string) => ({
-          product: {
-            connect: {
-              id: productId,
-            },
-          },
-        })),
-      },
-    },
-  });
-
-  const session = await stripe.checkout.sessions.create({
-    line_items,
-    mode: "payment",
-    billing_address_collection: "required",
-    phone_number_collection: {
-      enabled: true,
-    },
-    success_url: `${process.env.FRONTEND_STORE_URL}/cart?success=1`,
-    cancel_url: `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
-    metadata: {
-      orderId: order.id,
-    },
-  });
-
-  return NextResponse.json(
-    { url: session.url },
-    {
-      headers: corsHeaders,
-    }
-  );
+    return NextResponse.json(
+      { orderId: order.id, message: "Datos de pago guardados exitosamente" },
+      {
+        headers: corsHeaders,
+      }
+    );
+  } catch (error) {
+    console.error('Error al guardar datos de pago en la base de datos:', error);
+    return new NextResponse("Error al guardar datos en la base de datos", { status: 500 });
+  }
 }
