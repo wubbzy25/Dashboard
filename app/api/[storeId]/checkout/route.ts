fb5d3ba3fb5e5917d6988c7ff51134f9db89b389
Carlos Andres Salas Correa <83189141+wubbzy25@users.noreply.github.com>
2023-10-25 16:38:58 -0500

    Update route.ts

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -14,6 +14,7 @@ export async function OPTIONS() {
 export async function POST(req: Request,
   { params }: { params: { storeId: string } }) {
   const { address, phoneNumber, paymentMethod, productIds } = await req.json();
+  
 
   if (!address || !phoneNumber  || !productIds || productIds.length === 0) {
     return new NextResponse("Incomplete or invalid data", { status: 400 });
@@ -27,41 +28,23 @@ export async function POST(req: Request,
     }
   });
 
-  const line_items = [];
-
-  products.forEach((product) => {
-    line_items.push({
-      quantity: 1,
-      price_data: {
-        currency: 'COP',
-        product_data: {
-          name: product.name,
-        },
-        unit_amount: product.price.toNumber() * 100
-      }
-    });
-  });
-
-
   try {
     // Guarda los datos del formulario en tu base de datos
-    const order = await prismadb.order.create({
-      data: {
-        storeId: params.storeId,
-        address,
-        phone: phoneNumber,
-        isPaid: false,
-        orderItems: {
-          create: productIds.map((productId: string) => ({
-            product: {
-              connect: {
-                id: productId,
-              },
-            },
-          })),
-        },
-      },
-    });
+const order = await prismadb.order.create({
+    data: {
+      storeId: params.storeId,
+      isPaid: false,
+      orderItems: {
+        create: productIds.map((productId: string) => ({
+          product: {
+            connect: {
+              id: productId
+            }
+          }
+        }))
+      }
+    }
+  });
 
     return NextResponse.json(
       { orderId: order.id, message: "Datos de pago guardados exitosamente" },
