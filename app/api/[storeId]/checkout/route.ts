d40a217f2a81c42ed45ce07294953731a3eb0ccc
Carlos Andres Salas Correa <83189141+wubbzy25@users.noreply.github.com>
2023-10-25 13:10:06 -0500

    Update route.ts

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import prismadb from "@/lib/prismadb";
 
 const corsHeaders = {
-  "Access-Control-Allow-Origin": "https://store-3esknv2xy-wubbzy25.vercel.app",
+  "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
