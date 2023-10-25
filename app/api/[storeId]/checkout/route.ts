b418e5ca8e0eaef4ace02f6f2cfc1b3c7bed83ea
Carlos Andres Salas Correa <83189141+wubbzy25@users.noreply.github.com>
2023-10-25 17:05:53 -0500

    Update route.ts

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -32,20 +32,6 @@ export async function POST(
     }
   });
 
-  const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
-
-  products.forEach((product) => {
-    line_items.push({
-      quantity: 1,
-      price_data: {
-        currency: 'USD',
-        product_data: {
-          name: product.name,
-        },
-        unit_amount: product.price.toNumber() * 100
-      }
-    });
-  });
 
   const order = await prismadb.order.create({
     data: {
@@ -63,19 +49,6 @@ export async function POST(
     }
   });
 
-  const session = await stripe.checkout.sessions.create({
-    line_items,
-    mode: 'payment',
-    billing_address_collection: 'required',
-    phone_number_collection: {
-      enabled: true,
-    },
-    success_url: `${process.env.FRONTEND_STORE_URL}/cart?success=1`,
-    cancel_url: `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
-    metadata: {
-      orderId: order.id
-    },
-  });
 
   return NextResponse.json({ url: session.url }, {
     headers: corsHeaders
