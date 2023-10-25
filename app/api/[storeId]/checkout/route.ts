f11b9da1e0d4a3b27d92a1bfcc9c33bebbf7b8e0
wubbzy25 <carlosasalas321@gmail.com>
2023-10-24 22:16:36 -0500

    data changes

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -52,7 +52,7 @@ export async function POST(req: Request,
         phone: phoneNumber,
         isPaid: false,
         orderItems: {
-          create: productIds.map((productId) => ({
+          create: productIds.map((productId: string) => ({
             product: {
               connect: {
                 id: productId,
